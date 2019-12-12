2e7b3b9a452de414fab949d5c11e12b4e400abe5
Michał Bogucki <bogucek4@gazeta.pl>
2019-12-12 22:25:48 +0100

    sequential Code

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using BusinessLogic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private int n = 16;
+        [TestMethod]
+        public void InitializeCollectionTest()
+        {
+            
+            var linearSorting = new OddEvenSorting(n);
+            var result = linearSorting.GetPrimalColletion();
+            Equals(result.Count == n);
+        }
+
+        [TestMethod]
+        public void LinearSortCollectionTest()
+        {
+            var linearSorting = new OddEvenSorting(n);
+            var preResult = linearSorting.GetPrimalColletion();
+            var postResult = linearSorting.GetSortedSequentially();
+            var executionTime = linearSorting.sequentialTime;
+
+            if (n <= 16)
+                Console.WriteLine(string.Join(", ", preResult.ToArray()));
+
+            Console.WriteLine($"execution time = {executionTime} ms");
+
+            if (n <= 16)
+                Console.WriteLine(string.Join(", ", postResult.ToArray()));
+
+            Equals(preResult.Count == n);
+            Equals(postResult.Count == n);
+        }
+    }
+}
